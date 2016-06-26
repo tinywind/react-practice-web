86f881e52d2368c15cbf04bf417b07a31ffdefcc
tinywind <tinywind0@gmail.com>
2016-06-26 22:11:33 +0900

    coordinate by babel-codegen.

--- src/main/java/org/tinywind/reactpracticeweb/config/WebApplication.java
+++ src/main/java/org/tinywind/reactpracticeweb/config/WebApplication.java
@@ -1,5 +1,7 @@
 package org.tinywind.reactpracticeweb.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,11 +11,13 @@ import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver;
 
 @Configuration
 public class WebApplication extends WebMvcConfigurerAdapter {
+    private static Logger logger = LoggerFactory.getLogger(WebApplication.class);
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ScriptTemplateViewResolver viewResolver = new ScriptTemplateViewResolver();
         viewResolver.setPrefix("/templates/");
-        viewResolver.setSuffix(".jsx");
+        viewResolver.setSuffix(".js");
         registry.viewResolver(viewResolver);
         registry.scriptTemplate();
         super.configureViewResolvers(registry);
@@ -23,14 +27,17 @@ public class WebApplication extends WebMvcConfigurerAdapter {
     ScriptTemplateConfigurer configureScript() {
         ScriptTemplateConfigurer configurer = new ScriptTemplateConfigurer();
         configurer.setEngineName("nashorn");
-        configurer.setRenderFunction("renderJsx");
+        configurer.setRenderFunction("render");
         configurer.setScripts(
                 "/META-INF/resources/webjars/react/15.1.0/react.min.js",
                 "/META-INF/resources/webjars/react/15.1.0/react-dom-server.min.js",
-                "/build/react-templating.js",
+                "/build/react-templating.jsx",
                 "/static/js/doms.js",
                 "/static/js/playfield.js"
         );
+
+        logger.trace("  ScriptTemplateConfigurer configureScript()");
+
         return configurer;
     }
 }
